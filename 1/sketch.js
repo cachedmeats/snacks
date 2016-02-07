7d5ffd6d8925d61953c7bcf8b431b2da75952f96
cachedmeats <regoseattle@gmail.com>
2016-02-07 00:18:25 -0800

    Snack 1 prepared.

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,16 +1,37 @@
 var w = 255,
   h = 255;
 
-var osc;
+var bite, osc1, osc2, noise, reverb;
 var playing = false;
+var lips = document.getElementsByTagName("img");
+
+function preload() {
+  bite = loadSound('bite.mp3');
+}
 
 function setup() {
   createCanvas(w, h);
-  osc = new p5.Oscillator();
-  osc.setType('triangle');
-  osc.freq(240);
-  osc.amp(1);
-  osc.start();
+
+  bite.setVolume(1);
+
+  osc1 = new p5.Oscillator();
+  osc1.setType('sine');
+  osc1.freq(240);
+  osc1.amp(1);
+
+  osc2 = new p5.Oscillator();
+  osc1.setType('sine')
+  osc2.freq(480);
+  osc2.amp(1);
+  osc2.start();
+
+  reverb = new p5.Reverb();
+  reverb.process(osc1, 8, 80);
+  osc1.start();
+
+  noise = new p5.Noise('pink');
+  noise.amp(0.1);
+  noise.start();
 }
 
 var r, g, b;
@@ -33,8 +54,9 @@ function draw() {
   }
 }
 
+var mx, my;
+
 function mouseMoved() {
-  var mx, my;
 
   if (mouseX > w) {
     mx = w;
@@ -43,21 +65,43 @@ function mouseMoved() {
   } else {
     mx = mouseX;
   }
-  if (mouseY > w) {
+  if (mouseY > h) {
     my = h;
-  } else if (mousey < 0) {
-    my = 0; 
+  } else if (mouseY < 0) {
+    my = 0;
   } else {
     my = mouseY;
   }
 
-  osc.freq((mx + 40) * 2);
+  osc1.freq((mx + 30) * 2);
+  osc2.freq((my + 30) * 2);
 
   console.log('x: ' + mx + ', y: ' + my);
   b = mx;
   g = my;
 
   return false;
+}
+
+function mousePressed() {
+  osc1.stop();
+  osc2.stop();
+  
+  bite.play();
+  bite.loop();
+
+  lips[0].style.opacity = 1;
+}
+
+function mouseReleased() {
+  osc1.freq((mx + 30) * 2);
+  osc2.freq((my + 30) * 2);
+  osc1.start();
+  osc2.start();
+
+  bite.stop();
+
+  lips[0].style.opacity = 0;
 }
 
 function getRandomInt(min, max) {
